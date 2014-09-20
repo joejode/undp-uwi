88e905e11ea93b7a1153c2504183b19b36f5a5d5
joejode <whitehorne.jodiann@gmail.com>
2014-09-20 15:28:02 -0400

    changing double quotes to single quotes to pass jshint standard

--- app/scripts/script.js
+++ app/scripts/script.js
@@ -1,34 +1,39 @@
 function isReadyToSubmitForm() {
-    "use strict";
-    console.log(document.getElementById("userFname").value);
+    'use strict';
+    console.log(document.getElementById('userFname').value);
 }
 
 var showError = function (message) {
-    "use strict";
-    var error = document.getElementById("error-msg");
-    error.style.visibility = "visible";
+    'use strict';
+    var error = document.getElementById('error-msg');
+    error.style.visibility = 'visible';
     error.innerHTML = message;
 };
 
 var sendEmail = function (email) {
-    "use strict";
+    'use strict';
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (xmlhttp.readyState === 4) {
-            alert("Email sent");
+            alert('Email sent');
         }
     };
-    xmlhttp.open("GET", "http://cirrusdb.hostoi.com/sendEmail.php?email=" + email, true);
+    xmlhttp.open('GET', 'http://cirrusdb.hostoi.com/sendEmail.php?email=' + email, true);
     xmlhttp.send();
 };
 
 function validateForm() {
-    "use strict";
+    'use strict';
     var form = document.forms.signupForm, number = form.number1.value, email = form.email1.value;
     if (isNaN(number)) {
-        showError("Phone number must be a number");
+        showError('Phone number must be a number');
         return false;
     }
     sendEmail(email);
     console.log(number);
+}
+
+function registerUser() {
+    'use strict';
+
 }
